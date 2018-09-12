12cd1619ea49c85ab80c599c01ca757c7630877c
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2018-09-12 14:03:40 +0200

    added example counter

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="my-counter">
+    <button @click="decrement"> - </button>
+    <span>{{ count }}</span>
+    <button @click="increment"> + </button>
+    <br>
+    <span>{{ oddEven }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-counter',
+  props: [
+    'initialCount'
+  ],
+  data: function () {
+    return {
+      count: this.initialCount
+    }
+  },
+  computed: {
+    oddEven: function () {
+      return (this.count % 2 === 0 ? 'par' : 'impar')
+    }
+  },
+  watch: {
+    initialCount: function (newValue) {
+      this.count = newValue
+    }
+  },
+  methods: {
+    decrement () {
+      this.count -= 1
+      this.isCounterValueTen()
+    },
+    increment () {
+      this.count += 1
+      this.isCounterValueTen()
+    },
+    isCounterValueTen () {
+      if (this.count === 10) {
+        this.$emit('onCounterValueIsTen')
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <my-counter :initialCount="initialCount" @onCounterValueIsTen="myEvent"></my-counter>
+</template>
+
+<script>
+import MyCounter from '@/components/MyCounter.vue'
+
+export default {
+  name: 'counter',
+  data: function () {
+    return {
+      initialCount: 6
+    }
+  },
+  components: {
+    MyCounter
+  },
+  methods: {
+    myEvent () {
+      console.log('Soy el componente padre, el contador de mi componente hijo es 10')
+      this.initialCount = 0
+    }
+  }
+}
+</script>
