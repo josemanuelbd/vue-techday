17a322b8ebf4b8d08fd3b4fc2ab99ae60e1a8bcc
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2018-10-16 21:27:35 +0200

    rename CounterContainer

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Counter from './views/Counter.vue'
+import CounterContainer from './views/CounterContainer.vue'
 
 Vue.use(Router)
 
@@ -25,7 +25,7 @@ export default new Router({
     {
       path: '/counter',
       name: 'counter',
-      component: Counter
+      component: CounterContainer
     }
   ]
 })
